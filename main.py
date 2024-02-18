a72a4ab50c2e843518e728eb2cd0a543986fca74
cacapouh <yoshikti75@gmail.com>
2024-02-19 01:05:24 +0900

    Use bottle

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from flask import Flask
-from prometheus_flask_exporter import PrometheusMetrics
+from bottle import route, run, response
+from prometheus_client import start_http_server, Summary, generate_latest
+
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+count = 0
 
 
-app = Flask(__name__)
-metrics = PrometheusMetrics(app)
-
-
-@app.route('/')
+@route('/')
+@REQUEST_TIME.time()
 def index():
-    return {
-        'status': '200'
-    }
+    global count
+    count += 1
+    print("Request: {}".format(count))
+    return "version: 1.0.0"
 
 
-app.run(host='0.0.0.0', port=8080)
+@route('/metrics')
+def metrics():
+    response.content_type = 'text/plain; version=0.0.4; charset=utf-8'
+    return generate_latest()
+
+
+if __name__ == '__main__':
+    start_http_server(8000)
+    run(host='0.0.0.0', port=8080)
